2d73b2a46b79eca9fe0cc3f0dd1b3e0a053d7dc7
Rahul Vasantlal <220344548@aston.ac.uk>
2025-05-27 17:52:46 +0100

    complete question 5 : product of an array

--- JavaScript/Recurision/question.js
+++ JavaScript/Recurision/question.js
@@ -16,7 +16,34 @@ function factorial(n) {
     return n * factorial(n - 1);
 }
 
+// const allAreLessThanSeven = all(arr, function (num) {
+//     return num < 7;
+// });
+
+
+function productOfArrayIter(arr) {
+    total = 1
+    if (arr.length == 0) {
+        return 1
+    } else {
+        arr.forEach(element => {
+            total *= element
+        });
+    }
+    return total
+}
+
+function productOfArray(arr) {
+
+    if (arr.length == 0) return 1;
+    return arr.shift() * productOfArray(arr1)
+
+}
 
 n = 5
 e = 6
-console.log(factorial(n))
+arr1 = [1, 2, 3]
+arr2 = [1, 2, 3, 10]
+arr = [1, 10, 41, 27, 13, 38]
+console.log(productOfArray(arr2))
+
