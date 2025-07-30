912c82c92b64d838eda3637fbea23a18756b76ab
Rahul Vasantlal <220344548@aston.ac.uk>
2025-07-30 15:22:03 +0100

    complete sum object properties

--- Foundation_course/JavaScript_Basics/Objects/basics/js/basics.js
+++ Foundation_course/JavaScript_Basics/Objects/basics/js/basics.js
@@ -1,13 +1,16 @@
 
-function isEmpty(obj) {
-    for (key in obj) {
-        return false
-    }
-    return true
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
 }
 
-let schedule = {};
-console.log(isEmpty(schedule))
+function checkSalary(obj) {
+    let sum = 0
+    for (key in salaries) {
+        sum += salaries[key]
+    }
+    return sum
+}
 
-schedule["8:30"] = "get up";
-console.log(isEmpty(schedule))
+console.log(checkSalary(salaries))
