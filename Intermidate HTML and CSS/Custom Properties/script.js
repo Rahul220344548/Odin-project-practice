6d0ab1ab8ce04946f82ae6df49574bd662377f45
Rahul Vasantlal <220344548@aston.ac.uk>
2026-03-29 15:03:49 +0100

    practice dark and light theme buttons functionality using js

---
+++ Intermidate HTML and CSS/Custom Properties/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById('dark-theme-button').addEventListener('click', () => {
+    document.documentElement.style.setProperty
+        ('--background-color', ' #223a2c')
+})
+
+
+document.getElementById('light-theme-button').addEventListener('click', () => {
+    document.documentElement.style.setProperty
+        ('--background-color', '#D9CECC')
+})
